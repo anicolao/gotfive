90f5a206f61dd372c77aff111ddaffc0b441e6cf
Coder Agent <coder@conductor.local>
2026-05-05 21:33:40 +0000

    #12: Clean up build artifacts and ignore them

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BAsX6TH6.js","_app/immutable/chunks/Cu3uDmNK.js","_app/immutable/chunks/Dqj7RUA7.js","_app/immutable/chunks/DEDqjojZ.js"];
-export const stylesheets = [];
-export const fonts = [];
--- build/_app/immutable/entry/start.C0r1Alkg.js
+++
@@ -1,1 +0,0 @@
-import{o as e,t}from"../chunks/Dqj7RUA7.js";export{e as load_css,t as start};
