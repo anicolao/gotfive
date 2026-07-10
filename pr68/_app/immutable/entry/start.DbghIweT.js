58eb2ff5958af9f0ddb46fccf3f3f37741337022
anicolao <anicolao@users.noreply.github.com>
2026-07-10 22:55:36 +0000

    deploy: bb06f172d64c3b6f6d6ee84db2a603cd49496d9e

---
+++ pr68/_app/immutable/entry/start.DbghIweT.js
@@ -0,0 +1,1 @@
+import{o as e,t}from"../chunks/CUcnV8Am.js";export{e as load_css,t as start};
---
+++ pr68/_app/immutable/nodes/0.Bs6T7dkq.js
@@ -0,0 +1,1 @@
+import{b as e}from"../chunks/PWai8jUg.js";import{C as t,H as n,S as r,_ as i}from"../chunks/ByXgqJBZ.js";import"../chunks/DXLwiZ0H.js";var a=e({prerender:()=>!0,ssr:()=>!1,trailingSlash:()=>o}),o=`always`;function s(e,a){var o=t();i(n(o),()=>a.children),r(e,o)}export{s as component,a as universal};
